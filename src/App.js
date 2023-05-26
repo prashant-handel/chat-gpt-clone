af7495de3a1dcd0ede9d0a6a464f117772d71c7b
prashant_handel <prashanthandel2501@gmail.com>
2023-05-26 10:13:24 +0530

    Initialize Project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,96 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
 
-function App() {
+const App = () => {
+  const [message, setMessage] = useState(null);
+  const [value, setValue] = useState("");
+  const [previousChats, setPreviousChats] = useState([]);
+  const [currentTitle, setCurrentTitle] = useState(null);
+
+  const creatNewChat = ()=>{
+    setMessage(null)
+    setValue('')
+    setCurrentTitle(null)
+  }
+
+  const getMessages = async () => {
+    const options = {
+      method: "POST",
+      body: JSON.stringify({
+        message: value,
+      }),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    };
+    try {
+      const response = await fetch(
+        "http://localhost:5000/completions",
+        options
+      );
+      const data = await response.json();
+      setMessage(data.choices[0].message);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  useEffect(() => {
+    if (!currentTitle && value && message) {
+      setCurrentTitle(value);
+    }
+    if (currentTitle && value && message) {
+      setPreviousChats(
+        (prevChats) => (
+          [...prevChats,
+          {
+            title: currentTitle,
+            role: "user",
+            content: value,
+          },
+          {
+            title: currentTitle,
+            role: message.role,
+            content: message.content,
+          }]
+        )
+      );
+    }
+  }, [message, currentTitle]);
+  console.log(previousChats);
+
+  const currentChat = previousChats.filter(previousChats => (previousChats.title === currentTitle));
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <section className="side-bar">
+        <button onClick={creatNewChat}>+ New Chat</button>
+        <ul className="history">
+          <li>Blugh</li>
+        </ul>
+        <nav>
+          <p>Made by Prashant</p>
+        </nav>
+      </section>
+      <section className="main">
+        {!currentTitle && <h1>ChatGPT</h1>}
+        <ul className="feed">
+          {currentChat.map(chatMessage, index)=>}
+        </ul>
+        <div className="bottom-section">
+          <div className="input-container">
+            <input value={value} onChange={(e) => setValue(e.target.value)} />
+            <div id="submit" onClick={getMessages}>
+              ➢
+            </div>
+          </div>
+          <p className="info">
+            Free Research Preview. ChatGPT may produce inaccurate information
+            about people, places, or facts. ChatGPT May 12 Version.
+          </p>
+        </div>
+      </section>
     </div>
   );
-}
+};
 
 export default App;
